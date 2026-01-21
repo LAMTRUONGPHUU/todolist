12ea072a4364dfd2fa8fa3f4a0a6a72aa61cc478
Phu Lam Truong <ltphu@tma.com.vn>
2026-01-21 18:21:00 +0700

    fix: check connect mongo first

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,46 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors';
-import { connectDB } from './config/db.config.ts';
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import { errorHandler } from './middlewares/error.middleware.ts';
-import { AppConfig } from './config/app.config.ts';
-import console from 'node:console';
-import { sessionMiddleware } from './middlewares/session.middleware.ts';
-import { apiRouter } from './routes/api.route.ts';
-import passport from 'passport';
-import './config/passport.config.ts';
+import passport from "passport";
+
+import { connectDB } from "./config/db.config.ts";
+import { errorHandler } from "./middlewares/error.middleware.ts";
+import { sessionMiddleware } from "./middlewares/session.middleware.ts";
+import { apiRouter } from "./routes/api.route.ts";
+import { AppConfig } from "./config/app.config.ts";
+
 dotenv.config();
 
-const app = express();
-app.use(express.json());
-app.use(passport.initialize());
-app.use(sessionMiddleware);
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+async function bootstrap() {
+  try {
+    await connectDB(); // ⭐ CHỜ DB
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+    const app = express();
 
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use(passport.initialize());
+    app.use(sessionMiddleware);
 
-connectDB();
+    app.use(
+      cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+      })
+    );
 
-app.use("/api", apiRouter);
+    app.use("/api", apiRouter);
+    app.use(errorHandler);
 
-app.use(errorHandler);
+    app.listen(AppConfig.serverPort, () => {
+      console.log(`🚀 Server running on port ${AppConfig.serverPort}`);
+    });
+  } catch (err) {
+    console.error("❌ Failed to start server", err);
+    process.exit(1);
+  }
+}
 
-app.listen(AppConfig.serverPort, () => {
-  console.log('Server is running on port 3000');
-})
+bootstrap();
